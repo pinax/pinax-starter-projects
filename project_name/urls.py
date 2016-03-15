840e32faa00e22923948242fd1b3c25c3712b273
Graham Ullrich <graham@flyingcracker.com>
2016-03-14 19:07:00 -0600

    Updating starter project for Pinax 16.04.

--- project_name/urls.py
+++ project_name/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r"^admin/", include(admin.site.urls)),
-    url(r"", include("pinax.blog.urls")),
+    url(r"", include("pinax.blog.urls", namespace="pinax_blog")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
