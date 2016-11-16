c85b034aacf59b801ed7f6b896b801dd6168432b
Patrick Altman <paltman@gmail.com>
2016-11-16 06:46:56 -0600

    Bump Django release

--- project_name/settings.py
+++ project_name/settings.py
@@ -14,7 +14,9 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
