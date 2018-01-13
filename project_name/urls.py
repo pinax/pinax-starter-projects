5987d928207ca6a62924fc89012720b333083374
Patrick Altman <paltman@gmail.com>
2018-01-12 19:19:55 -0600

    Updates for Django 2.0

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
 ]
