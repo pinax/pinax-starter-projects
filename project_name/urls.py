511d05a5fdcca0073ef9732217267f97350a36c3
Patrick Altman <paltman@gmail.com>
2018-01-14 14:36:55 -0600

    Fix urls for 2.0

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,5 +7,5 @@ from django.contrib import admin
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pinax/waitinglist/list_signup.html"), name="home"),
     path("waitinglist/", include("pinax.waitinglist.urls", namespace="pinax_waitinglist")),
-    path("admin/", include(admin.site.urls)),
+    path("admin/", admin.site.urls),
 ]
