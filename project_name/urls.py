303eb4f0d05eebf3212a1c2ad52e35f82c64c7f3
Patrick Altman <paltman@gmail.com>
2018-01-14 14:10:37 -0600

    Fix urls for 2.0

--- project_name/urls.py
+++ project_name/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    path("admin/", include(admin.site.urls)),
+    path("admin/", admin.site.urls),
     path("", include("pinax.blog.urls", namespace="pinax_blog")),
 ]
 
