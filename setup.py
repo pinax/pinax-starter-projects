262c712da313e4ba4ee743654ddad5752d4a0268
Graham Ullrich <graham@flyingcracker.com>
2018-01-31 21:36:15 -0700

    Create setup.py
    A nicety for the developer.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="{{ project_name }}",
+    version="",
+    packages=find_packages(),
+    scripts=["manage.py"]
+)
