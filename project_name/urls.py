c4fcbef3656d056395440d4d1ff23f150bf793d3
Graham Ullrich <graham@flyingcracker.com>
2018-03-22 23:22:34 -0600

    Handle both Django 1.11 and 2.0
    Update pinax-templates requirement to latest (2.0.1) release.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+{% if django_version >= "2" %}from django.urls import path{% else %}from django.conf.urls import url{% endif %}
 from django.views.generic import TemplateView
 
 
-urlpatterns = [
+urlpatterns = [{% if django_version >= "2" %}
     path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
-]
+{% else %}
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+{% endif %}]
