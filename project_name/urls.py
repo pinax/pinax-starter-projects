17735690d66f8f273f77baf6317add5a3a8c1932
Graham Ullrich <graham@flyingcracker.com>
2018-03-24 14:51:30 -0600

    Handle both Django 1.11 and 2.0
    Use url() for v1.11, path() for v2.0.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,21 +1,25 @@
 from django.conf import settings
+{% if django_version < "2" %}from django.conf.urls import include, url{% endif %}
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.contrib import admin
+{% if django_version >= "2" %}from django.urls import include, path{% endif %}
 from django.views.generic import TemplateView
 
-from django.contrib import admin
-
 from .styleguides import StyleGuideView
-from .views import home
 
 
-urlpatterns = [
+urlpatterns = [{% if django_version >= "2" %}
     path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
     path("documents/", include("pinax.documents.urls", namespace="pinax_documents")),
-
-    path("style-guide/", StyleGuideView.as_view(), name="style-guide")
-]
+    path("style-guide/", StyleGuideView.as_view(), name="style-guide"),
+{% else %}
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^account/", include("account.urls")),
+    url(r"^documents/", include("pinax.documents.urls", namespace="pinax_documents")),
+    url(r"^style-guide/", StyleGuideView.as_view(), name="style-guide"),
+{% endif %}]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
