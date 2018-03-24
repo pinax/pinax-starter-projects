ff40a9b3f8cda40995c4e98e5ccd37af59d59930
Graham Ullrich <graham@flyingcracker.com>
2018-03-24 14:57:26 -0600

    Handle both Django 1.11 and 2.0
    Use url() for v1.11, path() for v2.0.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,16 +1,21 @@
 from django.conf import settings
-from django.urls import include, path
+{% if django_version < "2" %}from django.conf.urls import include, url{% endif %}
 from django.conf.urls.static import static
+from django.contrib import admin
+{% if django_version >= "2" %}from django.urls import include, path{% endif %}
 from django.views.generic import TemplateView
 
-from django.contrib import admin
 
-
-urlpatterns = [
+urlpatterns = [{% if django_version >= "2" %}
     path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
-    path("payments/", include("pinax.stripe.urls"))
-]
+    path("payments/", include("pinax.stripe.urls")),
+{% else %}
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^account/", include("account.urls")),
+    url(r"^payments/", include("pinax.stripe.urls")),
+{% endif %}]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
