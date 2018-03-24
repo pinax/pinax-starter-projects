5058bea09d0164455b518c2ff9ff3f820cc94b31
Graham Ullrich <graham@flyingcracker.com>
2018-03-24 14:38:33 -0600

    Handle both Django 1.11 and 2.0
    Use url() for v1.11, path() for v2.0.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,12 +1,16 @@
 from django.conf import settings
-from django.urls import include, path
+{% if django_version < "2" %}from django.conf.urls import include, url{% endif %}
 from django.conf.urls.static import static
-
 from django.contrib import admin
+{% if django_version >= "2" %}from django.urls import include, path{% endif %}
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
+
+urlpatterns = [{% if django_version >= "2" %}
     path("", include("pinax.blog.urls", namespace="pinax_blog")),
-]
+    path("admin/", admin.site.urls),
+{% else %}
+    url(r"^$", include("pinax.blog.urls", namespace="pinax_blog")),
+    url(r"^admin/", admin.site.urls),
+{% endif %}]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
