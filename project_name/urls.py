b282ba1457238c80006ea4fa4baada4708dedadf
Graham Ullrich <graham@flyingcracker.com>
2018-03-24 14:50:46 -0600

    Handle both Django 1.11 and 2.0
    Use url() for v1.11, path() for v2.0.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path, include
+{% if django_version < "2" %}from django.conf.urls import include, url{% endif %}
+from django.contrib import admin
+{% if django_version >= "2" %}from django.urls import include, path{% endif %}
 from django.views.generic import TemplateView
 
-from django.contrib import admin
 
-
-urlpatterns = [
+urlpatterns = [{% if django_version >= "2" %}
     path("", TemplateView.as_view(template_name="pinax/waitinglist/list_signup.html"), name="home"),
-    path("waitinglist/", include("pinax.waitinglist.urls", namespace="pinax_waitinglist")),
     path("admin/", admin.site.urls),
-]
+    path("waitinglist/", include("pinax.waitinglist.urls", namespace="pinax_waitinglist")),
+{% else %}
+    url(r"^$", TemplateView.as_view(template_name="pinax/waitinglist/list_signup.html"), name="home"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^waitinglist/", include("pinax.waitinglist.urls", namespace="pinax_waitinglist")),
+{% endif %}]
