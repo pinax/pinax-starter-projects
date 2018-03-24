4bb4f7cbdd145d967d32fc326a8185d326e477c5
Graham Ullrich <graham@flyingcracker.com>
2018-03-24 14:36:11 -0600

    Keep imports sorted correctly
    Use two separate conditionals for Django version-specific imports
    in order for imports to show in correct sort order.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-{% if django_version >= "2" %}from django.urls import include, path{% else %}from django.conf.urls import include, url{% endif %}
+{% if django_version < "2" %}from django.conf.urls import include, url{% endif %}
 from django.conf.urls.static import static
+{% if django_version >= "2" %}from django.urls import include, path{% endif %}
 from django.views.generic import TemplateView
 
 from django.contrib import admin
