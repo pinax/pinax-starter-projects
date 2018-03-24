cee56cfcb212c5ed2784776f97228f4b1cfe0c76
Graham Ullrich <graham@flyingcracker.com>
2018-03-23 18:18:29 -0600

    Add template rendering conditional for urls
    Use path() or url() based on Django version.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,15 +1,19 @@
 from django.conf import settings
-from django.urls import include, path
+{% if django_version >= "2" %}from django.urls import include, path{% else %}from django.conf.urls import include, url{% endif %}
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 from django.contrib import admin
 
 
-urlpatterns = [
+urlpatterns = [{% if django_version >= "2" %}
     path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
-]
+{% else %}
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^account/", include("account.urls")),
+{% endif %}]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
