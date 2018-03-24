10f36e3db910ba470e8e7f428eb30319cc8e9f18
Graham Ullrich <graham@flyingcracker.com>
2018-03-24 14:49:12 -0600

    Handle both Django 1.11 and 2.0
    Use url() for v1.11, path() for v2.0.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+{% if django_version < "2" %}from django.conf.urls import url{% endif %}
+{% if django_version >= "2" %}from django.urls import path{% endif %}
 
 
-urlpatterns = [
+urlpatterns = [{% if django_version >= "2" %}
     path("<str:path>", "{{ project_name }}.views.static_view"),
-]
+{% else %}
+    url(r"^(.*)$", "{{ project_name }}.views.static_view"),
+{% endif %}]
